115c5e314dfe0158faddd3ca26c57ed27df89303
renansilva42 <renansilvageografia@gmail.com>
2025-02-23 22:01:16 -0300

    ajustado um pouco o css, mas nao o suficiente

--- escola_manager/settings.py
+++ escola_manager/settings.py
@@ -37,7 +37,10 @@ ROOT_URLCONF = 'escola_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'admin'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
