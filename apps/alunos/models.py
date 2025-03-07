c1d4ccecdc35fdd35b1acae63dc04c771d8f49bc
renansilva42 <renansilvageografia@gmail.com>
2025-03-06 22:08:42 -0300

    finalizando para migracao do supabase

--- apps/alunos/models.py
+++ apps/alunos/models.py
@@ -1,44 +1,21 @@
-# apps/alunos/models.py
 from django.db import models
-from services.database import SupabaseService
+from django.utils import timezone
 
-
-class AlunoManager:
-    def __init__(self):
-        self.db = SupabaseService()
-
-    def create(self, **kwargs):
-        return self.db.create_aluno(kwargs)
-
-    def get(self, id):
-        return self.db.get_aluno(id)
-
-    def update(self, id, data):
-        return self.db.update_aluno(id, data)
-
-    def delete(self, id):
-        return self.db.delete_aluno(id)
-
-    def filter(self, **kwargs):
-        return self.db.list_alunos(kwargs)
-
-    def all(self):
-        return self.filter()  # Using the existing filter method without parameters
-
-class Aluno:
-    objects = AlunoManager()
-
-    TURNO_CHOICES = [
-        ('M', 'Manhã'),
-        ('T', 'Tarde'),
-    ]
-
-    NIVEL_CHOICES = [
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255, default='Não informado')
+    data_nascimento = models.DateField(default=timezone.now)
+    cpf = models.CharField(max_length=14, default='Não informado')
+    rg = models.CharField(max_length=20, default='Não informado')
+    foto = models.ImageField(upload_to='alunos/', null=True, blank=True)
+    nivel = models.CharField(max_length=3, choices=[
         ('EFI', 'Ensino Fundamental Anos Iniciais'),
         ('EFF', 'Ensino Fundamental Anos Finais'),
-    ]
-
-    ANO_CHOICES = [
+    ], default='EFI')
+    turno = models.CharField(max_length=1, choices=[
+        ('M', 'Manhã'),
+        ('T', 'Tarde'),
+    ], default='M')
+    ano = models.CharField(max_length=3, choices=[
         ('3', '3º Ano'),
         ('4', '4º Ano'),
         ('5', '5º Ano'),
@@ -47,25 +24,29 @@ class Aluno:
         ('8', '8º Ano'),
         ('901', '9º Ano - Turma 901'),
         ('902', '9º Ano - Turma 902'),
-    ]
+    ], default='3')
+    turma = models.CharField(max_length=10, default='A')
+    matricula = models.CharField(max_length=20, unique=True, default='0000')
+    email = models.EmailField(unique=True, null=True, blank=True)
+    telefone = models.CharField(max_length=15, default='Não informado')
+    endereco = models.CharField(max_length=255, default='Não informado')
+    cidade = models.CharField(max_length=100, default='Não informado')
+    uf = models.CharField(max_length=2, default='SP')
+    nome_responsavel1 = models.CharField(max_length=255, default='Não informado')
+    telefone_responsavel1 = models.CharField(max_length=15, default='Não informado')
+    nome_responsavel2 = models.CharField(max_length=255, blank=True, null=True)
+    telefone_responsavel2 = models.CharField(max_length=15, blank=True, null=True)
+    data_matricula = models.DateField(default=timezone.now)
+    observacoes = models.TextField(blank=True, null=True)
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __str__(self):
+        return self.nome
 
-class NotaManager:
-    def __init__(self):
-        self.db = SupabaseService()
+class Nota(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    disciplina = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=4, decimal_places=2)
+    data = models.DateField(auto_now_add=True)
 
-    def create(self, **kwargs):
-        return self.db.create_nota(kwargs)
-
-    def get_for_aluno(self, aluno_id):
-        return self.db.get_notas_aluno(aluno_id)
-
-class Nota:
-    objects = NotaManager()
-
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.disciplina}: {self.valor}"
