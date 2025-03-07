e4eb48c2c26b2bfbff899df70298fb4c1ed65722
renansilva42 <renansilvageografia@gmail.com>
2025-03-07 13:53:35 -0300

    atualizando sistema de visualizacao de alunos cadastrados

--- apps/alunos/urls.py
+++ apps/alunos/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.lista_alunos, name='lista_alunos'),
     path('buscar/', views.lista_alunos, name='buscar_alunos'),
-    path('aluno/<int:pk>/', views.detalhe_aluno, name='detalhe_aluno'),
+    # Altere esta linha para aceitar UUID
+    path('aluno/<str:pk>/', views.detalhe_aluno, name='detalhe_aluno'),
     path('cadastrar/', views.cadastrar_aluno, name='cadastrar_aluno'),
-    path('editar/<int:pk>/', views.editar_aluno, name='editar_aluno'),
-    path('excluir/<int:pk>/', views.excluir_aluno, name='excluir_aluno'),  # Adicione esta linha
-    path('adicionar_nota/<int:aluno_pk>/', views.adicionar_nota, name='adicionar_nota'),
-    path('aluno/<int:aluno_pk>/exportar_pdf/', views.exportar_detalhes_aluno_pdf, name='exportar_detalhes_aluno_pdf'),
+    path('editar/<str:pk>/', views.editar_aluno, name='editar_aluno'),
+    path('excluir/<str:pk>/', views.excluir_aluno, name='excluir_aluno'),
+    path('adicionar_nota/<str:aluno_pk>/', views.adicionar_nota, name='adicionar_nota'),
+    path('aluno/<str:aluno_pk>/exportar_pdf/', views.exportar_detalhes_aluno_pdf, name='exportar_detalhes_aluno_pdf'),
 ]
