449be566a06ea81464a5962230e58589838ed714
renansilva42 <renansilvageografia@gmail.com>
2025-03-09 12:15:41 -0300

    ajustes para salvar corretamente as fotos

--- escola_manager/urls.py
+++ escola_manager/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import home
-from django.contrib.auth import views as auth_views  # Adicione esta linha
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,11 +12,13 @@ urlpatterns = [
     path('chatbot/', include('apps.chatbot.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
     path('relatorios/', include('apps.relatorios.urls')),
-    
-    # Adicione estas linhas para incluir as URLs de autenticação do Django
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+# Configuração para servir arquivos de mídia em desenvolvimento
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Configuração para produção
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
