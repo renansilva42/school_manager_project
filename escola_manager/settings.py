388f77a3ff91208605699dc24e45d9ac5f612703
renansilva42 <renansilvageografia@gmail.com>
2025-03-10 22:29:18 -0300

    ajustando settings.py STATIC_URL

--- escola_manager/settings.py
+++ escola_manager/settings.py
@@ -223,9 +223,8 @@ LOGIN_REDIRECT_URL = '/'  # Isso redirecionará para a página inicial após o l
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
