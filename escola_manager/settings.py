7d7079eefec0e33a1155c03bececb0abfe50b5bf
renansilva42 <renansilvageografia@gmail.com>
2025-03-14 15:43:54 -0300

    ajustes para login

--- escola_manager/settings.py
+++ escola_manager/settings.py
@@ -256,7 +256,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-LOGIN_URL = '/usuarios/login/'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
