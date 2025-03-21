d72e205a0444f3049ac708b72ac074f75c88e4ba
renansilva42 <renansilvageografia@gmail.com>
2025-03-21 16:13:56 -0300

    corrigido /core/admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,22 +1,17 @@
 from django.contrib import admin
-from .models import Professor, SiteSettings
-from django.contrib import admin
-from .models import Professor, AtribuicaoDisciplina, DisponibilidadeHorario 
-
-admin.site.register(Professor)
-admin.site.register(AtribuicaoDisciplina)
-admin.site.register(DisponibilidadeHorario)
+from .models import Professor, SiteSettings, AtribuicaoDisciplina, DisponibilidadeHorario
 
 @admin.register(Professor)
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('nome', 'disciplina', 'email')
     search_fields = ('nome', 'disciplina')
 
-
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'school_name', 'contact_email')
     
     def has_add_permission(self, request):
-        # Impede a criação de múltiplas instâncias
         return SiteSettings.objects.count() == 0
+
+admin.site.register(AtribuicaoDisciplina)
+admin.site.register(DisponibilidadeHorario)
